8f1a2f085c4f2eb7673db5285a134e796d812455
MaionePietro <pietromaionespam@gmail.com>
2023-03-02 16:03:04 +0100

    FEAT main_here in MainApp

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -9,11 +9,9 @@
 </script>
 
 <template>
-    <h1>ciao mondo</h1>
+    
 </template>
 
 <style>
-    h1{
-        font-weight: bold;
-    }
+    
 </style>
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -9,11 +9,50 @@
 </script>
 
 <template>
-    <h1>ciao mondo</h1>
+    <section class="main_here">
+        <h2>Content goes here</h2>
+    </section>
+    <section class="main_cards">
+        <div class="card">
+            <a href="">
+                <img src="../../public/img/buy-comics-digital-comics.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </a>
+        </div>
+        <div class="card">
+            <a href="">
+                <img src="../../public/img/buy-comics-merchandise.png" alt="">
+                <span>DC MERCHANDISE</span>
+            </a>
+        </div>
+        <div class="card">
+            <a href="">
+                <img src="../../public/img/buy-comics-subscriptions.png" alt="">
+                <span>SUBSCRIPTION</span>
+            </a>
+        </div>
+        <div class="card">
+            <a href="">
+                <img src="../../public/img/buy-comics-shop-locator.png" alt="">
+                <span>COMIC SHOP LOCATOR</span>
+            </a>
+        </div>
+        <div class="card">
+            <a href="">
+                <img src="../../public/img/buy-dc-power-visa.svg" alt="">
+                <span>DC POWER VISA</span>
+            </a>
+        </div>
+    </section>
 </template>
 
 <style>
-    h1{
-        font-weight: bold;
+    .main_cards{
+        display: flex;
+        background-color: #0077F8;
+    }
+    a{
+        display: flex;
+        align-items: center;
     }
 </style>
