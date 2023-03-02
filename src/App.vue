3f5d40d6e35d23bc72a6086e1bad1721f7ec3f36
MaionePietro <pietromaionespam@gmail.com>
2023-03-02 15:27:29 +0100

    FEAT component header/main/footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
-<script >
+<script>
+  import HeaderApp from './components/HeaderApp.vue';
+  import MainApp from './components/MainApp.vue';
+  import FooterApp from './components/FooterApp.vue';
 
+  export default{
+    components:{
+      HeaderApp,
+      MainApp,
+      FooterApp
+    },
+        data(){
+            return{
+                
+            }
+        }
+    }
 </script>
 
 <template>
---
+++ src/components/MainApp.vue
@@ -0,0 +1,19 @@
+<script>
+    export default{
+        data(){
+            return{
+                
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>ciao mondo</h1>
+</template>
+
+<style>
+    h1{
+        font-weight: bold;
+    }
+</style>
