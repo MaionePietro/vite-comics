0c9ec338d1d7781f6d6b8de27f837f07cfa936fe
MaionePietro <pietromaionespam@gmail.com>
2023-03-02 15:49:42 +0100

    FEAT header_list in HeaderApp

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <script>
-  import HeaderApp from './components/HeaderApp.vue';
-  import MainApp from './components/MainApp.vue';
-  import FooterApp from './components/FooterApp.vue';
+import HeaderApp from './components/HeaderApp.vue';
+import MainApp from './components/MainApp.vue';
+import FooterApp from './components/FooterApp.vue';
 
-  export default{
-    components:{
-      HeaderApp,
-      MainApp,
-      FooterApp
-    },
-        data(){
-            return{
-                
-            }
-        }
+export default {
+  components: {
+    HeaderApp,
+    MainApp,
+    FooterApp
+  },
+  data() {
+    return {
+
     }
+  }
+}
 </script>
 
 <template>
- 
+  <HeaderApp></HeaderApp>
+  <MainApp></MainApp>
+  <FooterApp></FooterApp>
 </template>
 
 <style>
-
 </style>
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -9,11 +9,40 @@
 </script>
 
 <template>
-    <h1>ciao mondo</h1>
+    <section class="header_list">
+        <div class="logo">
+            <img src="../../public/img/dc-logo.png" alt="">
+        </div>
+        <div class="list">
+            <ul>
+                <li><a href="">CHARACTES</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIE</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAME</a></li>
+                <li><a href="">COLLECTIBLE</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+        </div>
+    </section>
 </template>
 
 <style>
-    h1{
-        font-weight: bold;
+    .header_list{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: white;
+    }
+    ul{
+        display: flex;
+        gap: 20px;
+        list-style: none;
+    }
+    a{
+        color: black;
     }
 </style>
